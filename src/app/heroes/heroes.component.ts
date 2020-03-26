2f0dc01f273c015dd14ea93a059cdade72192cd1
Daniel <danielfrancisco94@outlook.com>
2020-03-26 17:05:19 +0000

    removed dead code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
@@ -9,7 +8,6 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
   
   constructor(private heroService: HeroService) { }
 
